828da9bb7c808e591518ce47c4afba2bb3b79c06
Mogio99 <91249430+Mogio99@users.noreply.github.com>
2022-06-29 15:55:17 +0200

    Add files via upload

--- Ping.php
+++ Ping.php
@@ -1,53 +1,52 @@
-<?php
-    //Script that reading data from the HostList.csv file and constantly pings the IPs
-    //for check their status on the Lan/Internet
-    //Each line of the .csv file corresponds to a different Host/Device
-    //ex file.csv line "ip address","Nome Host","Tag href for redirect on WebPage's device " 
-
-
-    $iplist = array_map('str_getcsv', file('HostList.csv'));            //fill array
+<?php 
+    
+    
+    $iplist = array_map('str_getcsv', file('HostList.csv'));
     $i = count($iplist);
     $result = [];
 
-    for($j=0;$j<$i;$j++){                                               //loop for pinging every device in array
+    for($j=0;$j<$i;$j++){
         $ip = $iplist[$j][0];
-        $ping = exec("ping -n 1 $ip", $output,$status);                 //if status 0 = host online
-        $result[] = $status;                                            //if status 1 = host offline
+        $ping = exec("ping -n 1 $ip", $output,$status); //if status 0 = host online
+        $result[] = $status;                            //if status 1 = host offline
     }
 
-    //<html>                                                            create table for shown result
-    echo '<body bgcolor=FFFDD0>';
-    echo '<font face = Bebas Neue>';
-    echo ' <table border=1>
-            <th colspan = 5> Ping Status </th>
-            <tr>
-                <!-- <td>ID </td> -->
-                <td align=center width=150><b>IP/URL</td>
-                <td align=center width=200><b>Stato</td>
-                <td align=center width=200><b>Descrizione</td>
-                <td align=center width=200><b>Redirect</td>
-            </tr>    
-         ';
-        foreach($result as $temp => $k){                               //dynamic result in table                        
-            echo '<tr>';
-                
-                //echo '<td>'.$temp.'</td>';
-                echo '<td align=center width=100>'.$iplist[$temp][0].'</td>';
-                if($result[$temp]==0)
-                    echo '<td align=center width=100><p style=color:green><b>Host Online</b></p></td>';
-                else
-                    echo '<td align=center width=100><p style=color:red><b>Host Offline</b></p></td>';
-                echo '<td align=center width=100>'.$iplist[$temp][1].'</td>';
-                echo '<td align=center width=100>'.$iplist[$temp][2].'</td>';
-            
-            echo '</tr>';
-        }
-
-    echo "</table>";
-    echo '</font';        
-    echo'</body>';      
-    //</html>
-
-                                //refresh the page for pinging constantly
-    echo header("refresh:0.5"); //number of second   
+    echo '<html>
+            <head>
+                <title>Ping Checker</title>
+            </head>
+            <body bgcolor=FFFDD0>
+                <font face = Bebas Neue>
+                <table border=1>
+                        <th colspan = 6> Ping Status </th>
+                            <tr>
+                                <!-- <td>ID </td> -->
+                                <td align=center width=150><b>IP/URL</td>
+                                <td align=center width=150><b>MAC Address</td>
+                                <td align=center width=200><b>Nome Device</td>
+                                <td align=center width=200><b>Funzione</td>
+                                <td align=center width=200><b>Stato</td>               
+                                <td align=center width=200><b>Redirect</td>
+                            </tr>    
+         ';                 
+         header("refresh:1");
+    foreach($result as $temp => $k){
+    echo '                  <tr>';
+    echo '                      <td align=center width=100>'.$iplist[$temp][0].'</td>';
+    echo '                      <td align=center width=100>'.$iplist[$temp][1].'</td>';
+    echo '                      <td align=center width=100>'.$iplist[$temp][2].'</td>';
+    echo '                      <td align=center width=100>'.$iplist[$temp][3].'</td>';
+                            if($result[$temp]==0)
+    echo '                      <td align=center width=100><p style=color:green><b>Online</b></p></td>';
+                            else
+    echo '                      <td align=center width=100><p style=color:red><b>Offline</b></p></td>';                
+    echo '                      <td align=center width=100>'.$iplist[$temp][4].'</td>';   
+    echo '                  </tr>';
+    }
+    
+    echo '      </table>
+                </font>';  
+    //header("refresh:1");//per refreshare la pagina ed aggiornare gli stati metere il numero di secondi 
+    echo ' </body>      
+          </html>';                     
 ?>
