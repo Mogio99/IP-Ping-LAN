7878f4b56f7307d41d128034ebec26b2bb5ce503
Mogio99 <91249430+Mogio99@users.noreply.github.com>
2022-06-29 09:31:59 +0200

    Add files via upload

--- Ping.php
+++ Ping.php
@@ -1,22 +1,21 @@
 <?php
-    //require_once('HostList.php'); //for take the host list to ping
-    //include_once('HostList.php');
-    
-    /*esempio array
-    $iplist = array(
-        array("10.64.0.15 ","Switch 1 "),
-    );*/
-    $iplist = array_map('str_getcsv', file('HostList.csv'));
+    //Script that reading data from the HostList.csv file and constantly pings the IPs
+    //for check their status on the Lan/Internet
+    //Each line of the .csv file corresponds to a different Host/Device
+    //ex file.csv line "ip address","Nome Host","Tag href for redirect on WebPage's device " 
+
+
+    $iplist = array_map('str_getcsv', file('HostList.csv'));            //fill array
     $i = count($iplist);
     $result = [];
 
-    for($j=0;$j<$i;$j++){
+    for($j=0;$j<$i;$j++){                                               //loop for pinging every device in array
         $ip = $iplist[$j][0];
-        $ping = exec("ping -n 1 $ip", $output,$status); //if status 0 = host online
-        $result[] = $status;                            //if status 1 = host offline
+        $ping = exec("ping -n 1 $ip", $output,$status);                 //if status 0 = host online
+        $result[] = $status;                                            //if status 1 = host offline
     }
 
-    //<html>
+    //<html>                                                            create table for shown result
     echo '<body bgcolor=FFFDD0>';
     echo '<font face = Bebas Neue>';
     echo ' <table border=1>
@@ -29,7 +28,7 @@
                 <td align=center width=200><b>Redirect</td>
             </tr>    
          ';
-        foreach($result as $temp => $k){
+        foreach($result as $temp => $k){                               //dynamic result in table                        
             echo '<tr>';
                 
                 //echo '<td>'.$temp.'</td>';
@@ -42,7 +41,6 @@
                 echo '<td align=center width=100>'.$iplist[$temp][2].'</td>';
             
             echo '</tr>';
-
         }
 
     echo "</table>";
@@ -50,7 +48,6 @@
     echo'</body>';      
     //</html>
 
-                                //per refreshare la pagina ed aggiornare gli stati
-    echo header("refresh:0.5");   //metere il numero di secondi
-                                
+                                //refresh the page for pinging constantly
+    echo header("refresh:0.5"); //number of second   
 ?>
