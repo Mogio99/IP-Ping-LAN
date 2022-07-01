0032927e01f28084902d126efe4a468433a9d82e
Mogio99 <91249430+Mogio99@users.noreply.github.com>
2022-07-01 15:52:24 +0200

    Add files via upload

--- Ping.php
+++ Ping.php
@@ -1,14 +1,16 @@
 <?php   
-    $iplist = array_map('str_getcsv', file('pippo.csv'));
+    //array load
+    $iplist = array_map('str_getcsv', file('HostList.csv')); //change with your file .csv with eventualy is path
     $i = count($iplist);
     $result = [];
 
+    //loop for ping every host
     for($j=0;$j<$i;$j++){
         $ip = $iplist[$j][0];
-        $ping = exec("ping -n 1 $ip", $output,$status); //if status 0 = host online
-        $result[] = $status;                            //if status 1 = host offline
+        $ping = exec("ping -n 1 $ip", $output,$status); //if status 0 = ping done
+        $result[] = $status;                            //if status 1 = ping failur
     }
-
+    //html showing table, button and combox
     echo '<html>
             <head>
                 <title>Ping Checker</title>
@@ -20,29 +22,11 @@
                 <div class="timer" id="timer"></div><br><br>
 
                 <div id="DivInput">
-                    <select class="drop" id="dropdown" oninput="filterTable()" margin: auto;>
+                    <select class="drop" id="dropdown" oninput="filterTable()" autocomplete="off">
                         <option>Tutti</option>
-                        <option>Building Automation</option>
-                        <option>Computer</option>
-                        <option>Firewall</option>
-                        <option>Hypervisor</option>
-                        <option>Industrial Control System</option>
-                        <option>Information Technology</option>
-                        <option>IP Camera</option>
-                        <option>Network Access Control</option>
-                        <option>Networking</option>
-                        <option>Physical Security</option>
-                        <option>Process Instrumentation and Metrology</option>
-                        <option>Programmable Logic Controller</option>
-                        <option>Router or Switch</option>
-                        <option>Server</option>
-                        <option>Storage</option>
-                        <option>Surveillance</option>
                         <option>Switch</option>
-                        <option>Uninterruptible Power Supply</option>
-                        <option>Unknown</option>
-                        <option>Wireless Bridge</option>
-                        <option>Workstation</option>
+                        <option>Network Access Control</option>
+                        <option>Computer</option>
                     </select>
                   </div>
                   <div class="conteiner">
@@ -69,14 +53,14 @@
                                     <td><b>Redirect</td>
                                 </tr>    
          ';               
-    //header("refresh:30");
+    header("refresh:3"); //time for new ping (in second)
     foreach($result as $temp => $k){
     echo '                      <tr>';
-    echo '                          <td>'.$iplist[$temp][0].'</td>';
+    echo '                          <td>'.$iplist[$temp][0].'</td>';          //dynamic population of table, by taking array position
     echo '                          <td>'.$iplist[$temp][1].'</td>';
     echo '                          <td>'.$iplist[$temp][2].'</td>';
     echo '                          <td>'.$iplist[$temp][3].'</td>';
-                                if($result[$temp]==0)
+                                if($result[$temp]==0)                         //ping result, if 0 then is pinging properly
     echo '                          <td><p class="online">Online</p></td>';
                                 else
     echo '                          <td><p class="offline">Offline</p></td>';                
@@ -88,7 +72,7 @@
                 </div>
 
                 <script>
-
+                    //function for shown offline device first with button
                     function sortTableStatusOffF() {
                         var table, rows, switching, i, x, y, shouldSwitch;
                         table = document.getElementById("table");
@@ -114,7 +98,7 @@
                           }
                         }
                       }
-
+                      //function for shown online device first with button
                       function sortTableStatusOnF() {
                         var table, rows, switching, i, x, y, shouldSwitch;
                         table = document.getElementById("table");
@@ -140,7 +124,8 @@
                           }
                         }
                       }
-
+                      //function for sort table by IP with button
+                      //Uncomment the botton for use
                       function sortTableStatusIPCresc() {
                         var table, rows, switching, i, x, y, shouldSwitch;
                         table = document.getElementById("table");
@@ -167,6 +152,8 @@
                         }
                       }
 
+                      //function for sort table by IP decr with button
+                      //Uncomment the botton for use
                       function sortTableStatusIPDec() {
                         var table, rows, switching, i, x, y, shouldSwitch;
                         table = document.getElementById("table");
@@ -192,10 +179,10 @@
                           }
                         }
                       }
-                    
-                    function filterTable() {
-                        let dropdown, table, rows, cells, name, filter;
 
+                      //function for filter table by combobox choose
+                      function filterTable() {
+                        let dropdown, table, rows, cells, name, filter;
                         dropdown = document.getElementById("dropdown");
                         table = document.getElementById("table");
                         rows = table.getElementsByTagName("tr");
@@ -212,12 +199,13 @@
                           }
                         }
                       }
+                    
+                      //function for timer
+                      var timeLeft = 29;
+                      var elem = document.getElementById("timer");
+                      var timerId = setInterval(countdown, 1000);
 
-                    var timeLeft = 29;
-                    var elem = document.getElementById("timer");
-                    var timerId = setInterval(countdown, 1000);
-
-                    function countdown() {
+                      function countdown() {
                     if (timeLeft == -1) {
                         clearTimeout(timerId);
                     }else{
