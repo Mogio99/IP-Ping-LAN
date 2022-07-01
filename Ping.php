30876f26222af78de696c328faa1dd72047bf1a4
Mogio99 <91249430+Mogio99@users.noreply.github.com>
2022-07-01 14:11:58 +0200

    update css and button

--- Ping.php
+++ Ping.php
@@ -1,5 +1,5 @@
 <?php   
-    $iplist = array_map('str_getcsv', file('HostList.csv'));
+    $iplist = array_map('str_getcsv', file('pippo.csv'));
     $i = count($iplist);
     $result = [];
 
@@ -13,20 +13,46 @@
             <head>
                 <title>Ping Checker</title>
                 <link rel="stylesheet" href="style.css">
+                
             </head>
             <body>
                 <p class="timer">Tempo al prossimo Refresh/Ping:<p>
                 <div class="timer" id="timer"></div><br><br>
 
                 <div id="DivInput">
-                    <select class="drop" id="dropdown" oninput="filterTable()">
+                    <select class="drop" id="dropdown" oninput="filterTable()" margin: auto;>
                         <option>Tutti</option>
-                        <option>Switch</option>
-                        <option>Router or Switch</option>
+                        <option>Building Automation</option>
                         <option>Computer</option>
+                        <option>Firewall</option>
+                        <option>Hypervisor</option>
+                        <option>Industrial Control System</option>
+                        <option>Information Technology</option>
+                        <option>IP Camera</option>
+                        <option>Network Access Control</option>
+                        <option>Networking</option>
+                        <option>Physical Security</option>
+                        <option>Process Instrumentation and Metrology</option>
+                        <option>Programmable Logic Controller</option>
+                        <option>Router or Switch</option>
+                        <option>Server</option>
+                        <option>Storage</option>
+                        <option>Surveillance</option>
+                        <option>Switch</option>
+                        <option>Uninterruptible Power Supply</option>
+                        <option>Unknown</option>
+                        <option>Wireless Bridge</option>
+                        <option>Workstation</option>
                     </select>
-                    <p><button onclick="sortTableStatus()">Ordina per Stato</button></p>
-                </div>
+                  </div>
+                  <div class="conteiner">
+                    <div class="BCenter">
+                      <p><button class="OFF" onclick="sortTableStatusOffF()">Ordina per Offline</button></p>
+                      <p><button class="ON" onclick="sortTableStatusOnF()">Ordina per Online</button></p>
+                      <!--<p><button onclick="sortTableStatusIPDec()">Ordina per IP Decrescente</button></p>
+                      <p><button onclick="sortTableStatusIPCresc()">Ordina per IP Crescente</button></p>-->
+                    </div>
+                  </div>
 
                 <br>
                 <br>
@@ -35,16 +61,15 @@
                     <table id="table">
                             <th colspan=6> Ping Status </th>
                                 <tr>
-                                    <!-- <td>ID </td> -->
-                                    <td><b>IP/URL</td>
-                                    <td><b>MAC Address</td>
+                                    <td><b>Host</td>
+                                    <td><b>Indirizzo IP</td>
                                     <td><b>Nome Device</td>
-                                    <td><b>Funzione</td>
+                                    <td><b>Funzione Device</td>
                                     <td><b>Stato</td>               
                                     <td><b>Redirect</td>
                                 </tr>    
          ';               
-    header("refresh:30");
+    //header("refresh:30");
     foreach($result as $temp => $k){
     echo '                      <tr>';
     echo '                          <td>'.$iplist[$temp][0].'</td>';
@@ -64,7 +89,7 @@
 
                 <script>
 
-                    function sortTableStatus() {
+                    function sortTableStatusOffF() {
                         var table, rows, switching, i, x, y, shouldSwitch;
                         table = document.getElementById("table");
                         switching = true;
@@ -78,6 +103,84 @@
                             x = rows[i].getElementsByTagName("TD")[4];
                             y = rows[i + 1].getElementsByTagName("TD")[4];
                             if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                              shouldSwitch = true;
+                              break;
+                            }
+                          }
+                          if (shouldSwitch) {
+
+                            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                            switching = true;
+                          }
+                        }
+                      }
+
+                      function sortTableStatusOnF() {
+                        var table, rows, switching, i, x, y, shouldSwitch;
+                        table = document.getElementById("table");
+                        switching = true;
+                        
+                        while (switching) {
+                          switching = false;
+                          rows = table.rows;
+                          
+                          for (i = 1; i < (rows.length - 1); i++) {
+                            shouldSwitch = false;
+                            x = rows[i].getElementsByTagName("TD")[4];
+                            y = rows[i + 1].getElementsByTagName("TD")[4];
+                            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                              shouldSwitch = true;
+                              break;
+                            }
+                          }
+                          if (shouldSwitch) {
+
+                            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                            switching = true;
+                          }
+                        }
+                      }
+
+                      function sortTableStatusIPCresc() {
+                        var table, rows, switching, i, x, y, shouldSwitch;
+                        table = document.getElementById("table");
+                        switching = true;
+                        
+                        while (switching) {
+                          switching = false;
+                          rows = table.rows;
+                          
+                          for (i = 1; i < (rows.length - 1); i++) {
+                            shouldSwitch = false;
+                            x = rows[i].getElementsByTagName("TD")[1];
+                            y = rows[i + 1].getElementsByTagName("TD")[1];
+                            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                              shouldSwitch = true;
+                              break;
+                            }
+                          }
+                          if (shouldSwitch) {
+
+                            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                            switching = true;
+                          }
+                        }
+                      }
+
+                      function sortTableStatusIPDec() {
+                        var table, rows, switching, i, x, y, shouldSwitch;
+                        table = document.getElementById("table");
+                        switching = true;
+                        
+                        while (switching) {
+                          switching = false;
+                          rows = table.rows;
+                          
+                          for (i = 1; i < (rows.length - 1); i++) {
+                            shouldSwitch = false;
+                            x = rows[i].getElementsByTagName("TD")[1];
+                            y = rows[i + 1].getElementsByTagName("TD")[1];
+                            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
                               shouldSwitch = true;
                               break;
                             }
